596e56436c2ec66922fa1928ff00c4bc992e0b1c
yumee2 <miyazaki1707@gmail.com>
2026-02-01 12:27:00 +0700

    user repository interface created

--- internal/domain/entity/user.go
+++ internal/domain/entity/user.go
@@ -1,5 +1,11 @@
 package entity
 
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"time"
+)
+
 type User struct {
 	BaseModel
 
@@ -8,4 +14,21 @@ type User struct {
 	HashedPassword string `gorm:"not null"`
 
 	Boards []Board `gorm:"foreignKey:OwnerID"`
+}
+
+type RefreshToken struct {
+	BaseModel
+
+	TokenHash string    `gorm:"type:varchar(255);not null;uniqueIndex"`
+	UserID    string    `gorm:"type:uuid;not null;index"`
+	ExpiresAt time.Time `gorm:"not null"`
+}
+
+func (r *RefreshToken) HashToken(token string) {
+	hash := sha256.Sum256([]byte(token))
+	r.TokenHash = hex.EncodeToString(hash[:])
+}
+
+func (r *RefreshToken) IsValid() bool {
+	return time.Now().Before(r.ExpiresAt)
 }
